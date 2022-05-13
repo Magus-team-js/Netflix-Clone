5ce12fb20f7ccabe9925f849700c564cf3781827
BahaaNimer <bahaanimer97@gmail.com>
2022-05-13 06:23:44 +0300

    resize the images

--- src/components/modalMovie/ModalMovie.js
+++ src/components/modalMovie/ModalMovie.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
+
 
 
 
@@ -13,7 +13,7 @@ export default function ModalMovie(props) {
           <Modal.Title>{props.chosenMovie.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Card.Img variant="top" src={props.chosenMovie.poster_path} />
+          <img src={props.chosenMovie.poster_path} alt="Movie poster" />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={props.handleClose}>
--- src/components/movieList/movie/Movie.js
+++ src/components/movieList/movie/Movie.js
@@ -15,7 +15,7 @@ export default function Movie(props) {
   }
   return (
     <>
-      <Card style={{ width: '100%' }}>
+      <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={props.movie.poster_path} />
         <Card.Body>
           <Card.Title>{props.movie.title}</Card.Title>
